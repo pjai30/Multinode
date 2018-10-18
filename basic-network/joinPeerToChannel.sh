7d7d6765fd1ab87b693c16c0c477595fd9762258
pjai30 <31279809+pjai30@users.noreply.github.com>
2018-10-18 10:35:54 +0530

    Update joinPeerToChannel.sh

--- basic-network/joinPeerToChannel.sh
+++ basic-network/joinPeerToChannel.sh
@@ -22,9 +22,7 @@ export FABRIC_START_TIMEOUT=10
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
-#docker exec -e "CORE_PEER_LOCALMSPID=abgMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@abg.grasim.com/msp" -e "CORE_PEER_ADDRESS=peer0.abg.grasim.com:7051" peer0.abg.grasim.com peer channel join -b mychannel.block
 
-#docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" peer0.org1.example.com peer channel join -b mychannel.block
 
 # Create the channel
 docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel fetch config -o orderer.example.com:7050 -c mychannel
